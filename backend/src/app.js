2160e8a7eeed24373eeaa2218add609eea17b44a
dauryellen <dauryellen@live.com>
2021-01-21 19:00:01 -0300

    iniciando o arquivo de configuração app.js

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Rotas da API
+const index = require("./routes/index");
+// TODO: Declarar rota user.routes.js
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(index);
