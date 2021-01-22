a04e6030b76f5e31d6384dcd099e82c80ef94f29
dauryellen <dauryellen@live.com>
2021-01-21 21:59:21 -0300

    realizando a conexão com o banco de dados

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,28 @@ const morgan = require("morgan");
 
 const app = express();
 
+// importar o arquivo config.db.js
+const database = require("./config/db.config"); // conexão local da base de dados
+
+mongoose.Promise = global.Promise;
+
+// conexão da base de dados
+mongoose
+  .connect(database.local.localDatabaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log("A base de dados foi conectada com sucesso");
+    },
+    (err) => {
+      console.log(`Erro ao conectar com a base de dados...: ${err}`);
+      process(exit);
+    }
+  );
+
 // Rotas da API
 const index = require("./routes/index");
 // TODO: Declarar rota user.routes.js
