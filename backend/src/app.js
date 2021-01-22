b1d52f71702154bda793d8342753b7ea07e172e1
dauryellen <dauryellen@live.com>
2021-01-21 21:09:24 -0300

    ajustando config express

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -10,9 +9,9 @@ const app = express();
 const index = require("./routes/index");
 // TODO: Declarar rota user.routes.js
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
 app.use(cors());
 app.use(morgan("dev"));
 
